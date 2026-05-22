350033701590955f82bd37224038f7838fdb02f6
Ian Hunter <ianfhunter@gmail.com>
2026-05-22 08:37:53 +0100

    Use Google Sheets export for loot data

--- assets/js/loot.js
+++ assets/js/loot.js
@@ -1,5 +1,5 @@
 (function () {
-  var XLSX_URL = "data/RMMagicItems.xlsx";
+  var XLSX_URL = "https://docs.google.com/spreadsheets/d/1uRTtsACX1GNBvqgPS58iI-L-EB23ypCq/export?format=xlsx";
   var AVRAE_JSON_URL = "data/avrae-item-export.json";
   var PAGE_SIZE = 100;
   var CART_STORAGE_KEY = "rm-loot-cart-v1";
